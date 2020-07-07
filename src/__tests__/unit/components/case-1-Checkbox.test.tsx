cf656a535752ad2706e1fdbad62a17a1d2877d88
Frantz Kati <bahdcoder@gmail.com>
2020-07-07 22:35:27 +0100

    1-test-a-component-with-react-dom

--- src/__tests__/unit/components/case-1-Checkbox.test.tsx
+++ src/__tests__/unit/components/case-1-Checkbox.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { axe } from 'jest-axe'
 import { render, fireEvent } from '@testing-library/react'
 
@@ -11,7 +12,24 @@ import Checkbox from '../../../components/Checkbox'
  * should.
  */
 describe('The <Checkbox /> component', () => {
-  it('❌ Should render the label and checkbox the user will see', () => {})
+  const defaultCheckboxProps = {
+    label: 'TEST_CHECKBOX_LABEL',
+    id: 'TEST_CHECKBOX_ID',
+    background: '#000',
+    checkMarkBackground: '#fff',
+    onChange: jest.fn(),
+    checked: false,
+  }
+  it('Should render the label and checkbox the user will see', () => {
+    const container = document.createElement('div')
+    ReactDOM.render(
+      <Checkbox {...defaultCheckboxProps} />,
+      container
+    )
+
+    expect(container.querySelector('label')).not.toBeNull()
+    expect(container.querySelector('input[type="checkbox"]')).not.toBeNull()
+  })
 
   it('❌ Should make the checkbox accessible by setting the id and htmlFor attributes on label and checkbox', () => {})
 
