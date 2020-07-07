39c3b663f663c9921ddd8ddd731cc3fe46ace297
Frantz Kati <bahdcoder@gmail.com>
2020-07-08 00:53:03 +0100

    15-integration-test-for-fetch-products

--- src/__tests__/integration/app.test.tsx
+++ src/__tests__/integration/app.test.tsx
@@ -8,8 +8,37 @@ import App from '../../components/App'
 import { createStore } from '../../store'
 import { FiltersWrapper } from '../../components/FiltersWrapper'
 
+jest.mock('../../helpers/axios')
+
+const mockAxios = Axios as any
+
 describe('The app ', () => {
-  test('❌ it fetches and renders all products on the page', async () => {})
+  const setupApp = () => render(
+    <StoreProvider store={createStore()}>
+      <FiltersWrapper>
+        <App />
+      </FiltersWrapper>
+    </StoreProvider>
+  )
+
+  test('it fetches and renders all products on the page', async () => {
+    mockAxios.get.mockResolvedValue({
+      data: [{
+        id: 1,
+        name: 'test name 1',
+        image: 'image-1.png',
+        price: 'from $22.99'
+      }, {
+        id: 2,
+        name: 'test name 2',
+        image: 'image-2.png',
+        price: 'from $29.99'
+      }]
+    })
+    const { findAllByTestId } = setupApp()
+
+    expect(await findAllByTestId('ProductTile')).toHaveLength(2)
+  })
 
   test('❌ it can open and close the filters panel', async () => {})
 
